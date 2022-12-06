8cc36c3fcd515c449b4725ec1bdffe616922a820
ekadk <ekadharmakusuma3@gmail.com>
2022-12-06 20:07:29 +0800

    refactor: move port listen from app to www

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express()
-const port = 3000
 
 app.use(express.json())
 
@@ -8,6 +7,4 @@ app.get('/', (req, res) => {
   res.status(200).json({ message: 'hello world!' })
 })
 
-app.listen(port, () => {
-  console.log(`Sewa Motor App is running on port ${port}`);
-})
+module.exports = app

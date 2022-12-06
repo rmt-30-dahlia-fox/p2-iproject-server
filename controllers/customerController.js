a2be24b5f1975a7fe6365c088c22934ed43e1f19
Joshua Natanael <joshuanatanael@Joshuas-MacBook-Air.local>
2022-12-06 12:27:06 +0700

    add register login finished

--- controllers/customerController.js
+++ controllers/customerController.js
@@ -1,7 +1,51 @@
+const { comparePassword } = require('../helpers/bcrypt');
+const { generateToken } = require('../helpers/jwt');
 const {User, Car, Dealer, Transaction, Review} = require('../models');
 
 class customerController{
-  
+  static async registerAccount(req, res, next){
+    try {
+      const {firstName, lastName, email, password, phoneNumber} = req.body;
+      const newUser = await User.create(
+        {firstName, lastName, email, password, phoneNumber}
+      )
+      res.status(201).json({
+        id: newUser.id,
+        email: newUser.email,
+      })
+    } catch (error) {
+      next(error);
+    }
+  }
+  static async loginAccount(req, res, next){
+    try {
+      const {email, password} = req.body;
+      if(!email){
+        throw("noEmail")
+      }
+      if(!password){
+        throw("noPassword")
+      }
+      const user = await User.findOne({
+        where: {
+          email
+        }
+      })
+      if(!user){
+        throw("invalid")
+      }
+      const isValidPassword = comparePassword(password, user.password);
+      if(!isValidPassword){
+        throw("invalid");
+      }
+      const payload = {
+        id: user.id
+      }
+      res.status(200).json({access_token: generateToken(payload)})
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = customerController;

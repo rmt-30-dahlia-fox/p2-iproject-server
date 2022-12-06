06f00033533aea018dca99b5aa0dcf38f91234fe
Joshua Natanael <joshuanatanael@Joshuas-MacBook-Air.local>
2022-12-06 12:23:27 +0700

    add initial setup for customer routes

---
+++ controllers/customerController.js
@@ -0,0 +1,7 @@
+const {User, Car, Dealer, Transaction, Review} = require('../models');
+
+class customerController{
+  
+}
+
+module.exports = customerController;
---
+++ routes/customerRouter.js
@@ -0,0 +1,6 @@
+const customerController = require('../controllers/customerController');
+const router = require('express').Router();
+
+
+
+module.exports = router;

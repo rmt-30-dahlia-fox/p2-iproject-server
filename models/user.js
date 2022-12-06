f535a4dd574aecb489581d9eeef33b9f3b4f7ce9
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-12-06 21:08:49 +0700

    create User, Movie, Bookmark table & model

---
+++ models/user.js
@@ -0,0 +1,58 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+const { USE } = require('sequelize/types/index-hints');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+      User.hasMany(models.Bookmark, {
+        foreignKey: "UserId"
+      })
+    }
+  }
+  User.init({
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Username is required" },
+        notEmpty: { msg: "Username is required" },
+      },
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: {
+        msg: "Email already exist"
+      },
+      unique: true,
+      validate: {
+        notNull: { msg: "Email is required" },
+        notEmpty: { msg: "Email is required" },
+        isEmail: { msg: "Invalid email format" },
+        // async unique(value) {
+        //   const findUser = await User.findOne({ where: { email: value } });
+        //   if (findUser) throw new Error("Email already exist");
+        // },
+      },
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        notNull: { msg: "Password is required" },
+        notEmpty: { msg: "Password is required" },
+      },
+    }
+  }, {
+    sequelize,
+    modelName: 'User',
+  });
+  return User;
+};

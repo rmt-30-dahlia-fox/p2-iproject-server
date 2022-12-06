e5647139124fe9394c8d4be1490bd75ee4d2576a
alammooo <abdullah.alamm@gmail.com>
2022-12-06 21:00:38 +0700

    add router : covid-data from third party API

---
+++ controllers/controller.js
@@ -0,0 +1,16 @@
+const axios = require("axios")
+class Controller {
+  static async getCovidData(req, res, next) {
+    try {
+      const { data } = await axios({
+        url: "https://data.covid19.go.id/public/api/prov.json",
+        method: "GET",
+      })
+      res.status(200).json(data)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+}
+
+module.exports = Controller

ed73c462f61aea1d10d41d0539ce123de838370d
ekadk <ekadharmakusuma3@gmail.com>
2022-12-06 22:21:01 +0800

    modify app

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const app = express()
+const cors = require('cors');
+const router = require('./routes');
+const errorHandler = require('./middlewares/errorHandler');
 
+app.use(cors())
+app.use(express.urlencoded({ extended: false }))
 app.use(express.json())
-
-app.get('/', (req, res) => {
-  res.status(200).json({ message: 'hello world!' })
-})
+app.use(router)
+app.use(errorHandler)
 
 module.exports = app

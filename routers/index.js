5e1a248eb9b3eddb2d092f1a1c2407456bf77fe5
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-12-07 02:13:48 +0700

    use auth

--- routers/index.js
+++ routers/index.js
@@ -3,7 +3,7 @@ const movieRouter = require("./movies");
 const bookmarkRouter = require("./bookmark");
 // const paymentRouter = require("./payment");
 const Controller = require("../controllers");
-// const authentication = require("../middlewares/authentication");
+const authentication = require("../middlewares/authentication");
 
 router.post("/social-login", Controller.socialLogin);
 router.post("/register", Controller.register);
@@ -11,7 +11,7 @@ router.post("/login", Controller.login);
 
 router.use("/movies", movieRouter);
 
-// router.use(authentication);
+router.use(authentication);
 
 // router.use("/payment", paymentRouter);
 router.use("/bookmark", bookmarkRouter);

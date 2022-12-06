8e5908e69894025868c29f741a99c1ee21fa51ff
Aldrino Wibowo <mraldrino@gmail.com>
2022-12-06 15:06:09 +0700

    init project

---
+++ models/cart.js
@@ -0,0 +1,28 @@
+"use strict";
+const { Model } = require("sequelize");
+module.exports = (sequelize, DataTypes) => {
+  class Cart extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Cart.init(
+    {
+      UserId: DataTypes.INTEGER,
+      name: DataTypes.STRING,
+      imageUrl: DataTypes.STRING,
+      level: DataTypes.STRING,
+      price: DataTypes.STRING,
+    },
+    {
+      sequelize,
+      modelName: "Cart",
+    }
+  );
+  return Cart;
+};

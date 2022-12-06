b0fce7424a3bb2acadec97a87291ea196c264dbc
ekadk <ekadharmakusuma3@gmail.com>
2022-12-06 20:01:03 +0800

    add: helpers: jwt & bcrypt

---
+++ helpers/jwt.js
@@ -0,0 +1,12 @@
+const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET
+
+function signToken(payload) {
+  return jwt.sign(payload, SECRET)
+}
+
+function verifyToken(token) {
+  return jwt.verify(token, SECRET)
+}
+
+module.exports = { signToken, verifyToken }

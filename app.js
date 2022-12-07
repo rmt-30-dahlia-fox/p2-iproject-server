ed2874461dab1dc3f71859f1b99393bd735cf3d2
Joshua Natanael <joshuanatanael@Joshuas-MacBook-Air.local>
2022-12-07 22:08:10 +0700

    setting deployment finished

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
-require('dotenv').config()
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config();
+}
 const express = require('express');
 const errorHandler = require('./middlewares/errorHandler');
 const router = require('./routes');
 const cors = require('cors')
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());

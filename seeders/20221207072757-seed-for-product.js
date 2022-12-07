90fe5ec18cac99dc8005600e917c6b01d00d69f8
Qomar034 <nurqomarudin034@gmail.com>
2022-12-07 14:50:29 +0700

    Create Helpers and dotenv, success seeding

--- seeders/20221207072757-seed-for-product.js
+++ seeders/20221207072757-seed-for-product.js
@@ -3,23 +3,14 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+    let data = require('../data.json').Products.map(el => {
+      el.createdAt = el.updatedAt = new Date()
+      return el
+    })
+    await queryInterface.bulkInsert("Products", data)
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    await queryInterface.bulkDelete('Products', null, {});
   }
 };

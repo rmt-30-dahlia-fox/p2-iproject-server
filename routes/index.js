86a4279f349a9a1b5a931332657fdf86acd6898f
ekadk <ekadharmakusuma3@gmail.com>
2022-12-07 14:23:10 +0800

    fix: admin main routes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const router = express.Router();
 const adminRouter = require('./admin');
 const customerRouter = require('./customer');
 
-router.use('/', adminRouter)
+router.use('/admin', adminRouter)
 router.use('/customer', customerRouter)
 
 module.exports = router;

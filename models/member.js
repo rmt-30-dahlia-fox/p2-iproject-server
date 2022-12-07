111ec692b8cb464fb6994d1e61b00f70255ee109
Qomar034 <nurqomarudin034@gmail.com>
2022-12-07 14:29:33 +0700

    Migrating User, Member, Category, Product, History, and Create Seed

---
+++ models/member.js
@@ -0,0 +1,28 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Member extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  }
+  Member.init({
+    name: DataTypes.STRING,
+    gender: DataTypes.STRING,
+    email: DataTypes.STRING,
+    phone: DataTypes.STRING,
+    point: DataTypes.INTEGER,
+    cashierId: DataTypes.INTEGER
+  }, {
+    sequelize,
+    modelName: 'Member',
+  });
+  return Member;
+};

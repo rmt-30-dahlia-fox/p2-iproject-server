4a103cbb7140ab6bbfe03de77e3ab4afd84c6a4a
Qomar034 <nurqomarudin034@gmail.com>
2022-12-07 15:33:36 +0700

    Create Register, Authentication, and ErrorHandler

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,10 @@
 const router = require('express').Router()
 const Controller = require('../controllers/controller')
+const Authentication = require('../middlewares/authentication')
 
 router.post('/login', Controller.postLogin)
+
+router.use(Authentication)
+router.post('/register', Controller.postRegister)
 
 module.exports = router

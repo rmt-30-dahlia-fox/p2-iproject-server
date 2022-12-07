9bef55c1145992e7f20addbdbcbda468d992d8c6
setyapanggaraka <setyarakap02@gmail.com>
2022-12-08 00:30:56 +0700

    add authentication, add error handler

---
+++ middlewares/errorHandler.js
@@ -0,0 +1,42 @@
+const errorHandler = (err, req, res, next) => {
+  let code = 500;
+  let message = "Internal server error";
+  console.log(err);
+
+  if (err.name === "Email is required") {
+    code = 400;
+    message = err.name;
+  } else if (err.name === "Password is required") {
+    code = 400;
+    message = err.name;
+  } else if (err.name === "Invalid email/password") {
+    code = 401;
+    message = err.name;
+  } else if (err.name === "Invalid Token" || err.name === "JsonWebTokenError") {
+    code = 401;
+    message = "Invalid Token";
+  } else if (err.name === "Doctor not found") {
+    code = 404;
+    message = err.name;
+  } else if (
+    err.name === "SequelizeValidationError" ||
+    err.name === "SequelizeUniqueConstraintError"
+  ) {
+    code = 404;
+    const error = err.errors[0].message;
+    message = error;
+  } else if (err.name === "Medicine not found") {
+    code = 404;
+    message = err.name;
+  } else if (err.name === "Amount is required") {
+    code = 401;
+    message = err.name;
+  } else if (err.name === "Medicine is not enough") {
+    code = 400;
+    message = err.name;
+  }
+
+  res.status(code).json({ message });
+};
+
+module.exports = errorHandler;
--- router/index.js
+++ router/index.js
@@ -1,52 +1,12 @@
 const express = require("express");
 const Controller = require("../Controller/controller");
+const authentication = require("../middlewares/authentication");
+const errorHandler = require("../middlewares/errorHandler");
 const router = express.Router();
-
-router.get("/", (req, res) => {
-  res.send("masuk");
-});
 
 router.post("/login", Controller.login);
 router.post("/register", Controller.register);
+router.use(authentication);
 
-router.use((err, req, res, next) => {
-  let code = 500;
-  let message = "Internal server error";
-  console.log(err);
-
-  if (err.name === "Email is required") {
-    code = 400;
-    message = err.name;
-  } else if (err.name === "Password is required") {
-    code = 400;
-    message = err.name;
-  } else if (err.name === "Invalid email/password") {
-    code = 401;
-    message = err.name;
-  } else if (err.name === "Invalid Token" || err.name === "JsonWebTokenError") {
-    code = 401;
-    message = "Invalid Token";
-  } else if (err.name === "Doctor not found") {
-    code = 404;
-    message = err.name;
-  } else if (
-    err.name === "SequelizeValidationError" ||
-    err.name === "SequelizeUniqueConstraintError"
-  ) {
-    code = 404;
-    const error = err.errors[0].message;
-    message = error;
-  } else if (err.name === "Medicine not found") {
-    code = 404;
-    message = err.name;
-  } else if (err.name === "Amount is required") {
-    code = 401;
-    message = err.name;
-  } else if (err.name === "Medicine is not enough") {
-    code = 400;
-    message = err.name;
-  }
-
-  res.status(code).json({ message });
-});
+router.use(errorHandler);
 module.exports = router;

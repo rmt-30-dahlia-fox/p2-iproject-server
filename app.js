34f0fca5e000cd897ddaf1640a50f6a5e0b898e8
setyapanggaraka <setyarakap02@gmail.com>
2022-12-08 01:31:23 +0700

    upload success

--- app.js
+++ app.js
@@ -2,8 +2,12 @@ const express = require("express");
 const router = require("./router");
 const app = express();
 const port = 3000;
+const cors = require("cors");
 
+app.use(cors());
 app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
 app.use(router);
 
 app.listen(port, () => {

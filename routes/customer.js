f99fdab84ad1b640b9ff4076044b19fec2901819
ekadk <ekadharmakusuma3@gmail.com>
2022-12-07 15:26:57 +0800

    fix: authentication

--- routes/customer.js
+++ routes/customer.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const CustomerController = require("../controllers/customer");
+const Auth = require('../middlewares/authentication');
 const router = express.Router();
 
 router.post("/register", CustomerController.register);
 router.post("/login", CustomerController.login);
-
-// router.use(Auth.admin)
-
 router.get("/units", CustomerController.getAllUnits);
 router.get("/units/:id", CustomerController.getUnitById);
 
+router.use(Auth.customer)
+
 router.get("/orders", CustomerController.getAllOrders);
 router.get("/orders/:id", CustomerController.getOrderById)
+router.post("/orders/:unitId", CustomerController.postOrder)
 router.delete("/orders/:id", CustomerController.deleteOrderById)
 
 module.exports = router;

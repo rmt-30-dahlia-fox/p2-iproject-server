637850356ec905eff996dede4526ce9db9b0223f
ekadk <ekadharmakusuma3@gmail.com>
2022-12-08 23:49:38 +0800

    add: customer authorization

---
+++ middlewares/authorization.js
@@ -0,0 +1,18 @@
+const { Order } = require("../models");
+
+module.exports = class Authz {
+  static async customer(req, res, next) {
+    try {
+      const id = req.params.id;
+      const UserId = req.user.id;
+
+      const order = await Order.findByPk(id);
+      if (!order) throw { name: "order_not_found" };
+      if (order.UserId !== UserId) throw { name: "forbidden" };
+
+      next()
+    } catch (err) {
+      next(err);
+    }
+  }
+};

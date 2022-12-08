29884d20e9cb2f018d99b75296fa9ddac6b73134
Bobby Rahdyan <bobby.notokoesoemo@gmail.com>
2022-12-08 11:21:10 +0700

    main

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
+// console.log('test');
 app.use(router)
 app.use(errorHandler)
 
